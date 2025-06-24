2be7b21bc45b9476303087fad2d1fa89b25021a8
Quintex9 <misko.smatana@gmail.com>
2025-06-24 04:57:33 +0200

    Pridanie logiky do Results

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 
 function App() {
   const [userInput, setUserInput] = useState({
@@ -14,7 +15,7 @@ function App() {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue,//+newValue, zmena stringu na číslo
       };
     });
   }
@@ -23,6 +24,7 @@ function App() {
     <>
       <Header/>
       <UserInput userInput={userInput} onChange={handleChange}/>
+      <Results userInput={userInput}/>
     </>
   )
 }
